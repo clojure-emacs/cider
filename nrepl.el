727fd31f1a533fd12ce1ca0b16b5e48785ea309b
Tim King <kingtim@gmail.com>
2012-06-28 07:59:22 -0700

    humble beginnings to C-c M-p nrepl-set-ns

--- nrepl.el
+++ nrepl.el
@@ -1,5 +1,5 @@
 ;;; nrepl.el --- Client for Clojure nrepl -*- lexical-binding: t -*-
-;;
+
 ;;;; License
 ;; Copyright © 2012 Phil Hagelberg, Tim King
 ;; Authors: Tim King <kingtim@gmail.com>
@@ -8,34 +8,34 @@
 ;; Version: 0.0.1
 ;; Keywords: languages, clojure, nrepl
 ;; Package-Requires: ((clojure-mode "1.7"))
-;;
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-;;
+
 ;; This file is not part of GNU Emacs.
-;;
+
 ;;; Commentary:
-;;
+
 ;; Provides an elisp client to connect to nrepl servers.
-;;
+
 ;;; Installation:
-;;
+
 ;; For now, M-x package-install-file or M-x package-install-from-buffer.
-;;
+
 ;;; Usage:
-;;
+
 ;; M-x nrepl-jack-in
-;;
+
 ;;; Code:
 
 (require 'clojure-mode)
@@ -532,6 +532,7 @@ DIRECTION is 'forward' or 'backward' (in the history list)."
     (define-key map (kbd "C-c C-e") 'nrepl-eval-last-expression)
     (define-key map (kbd "C-c C-m") 'nrepl-macroexpand-1-last-expression)
     (define-key map (kbd "C-c M-m") 'nrepl-macroexpand-last-expression)
+    (define-key map (kbd "C-c M-p") 'nrepl-set-ns)
     map))
 
 (defvar nrepl-mode-map
@@ -877,6 +878,24 @@ balanced."
         (save-excursion
           (goto-char start)
           (insert ";;; output cleared"))))))
+
+(defun nrepl-current-ns ()
+   "Return the ns package in the current context.
+ If `nrepl-buffer-ns' has a value then return that, otherwise
+ search for and read a `ns' form."
+   (let ((ns nrepl-buffer-ns))
+     (or (and (string= ns "user")
+              (save-restriction
+                (widen)
+                (clojure-find-ns)))
+         ns)))
+
+(defun nrepl-set-ns (ns)
+  "Switch the namespace of the nREPL buffer to ns."
+  (interactive (list (let ((ns (nrepl-current-ns)))
+                       (read-string "Namespace: " ns nil))))
+  (with-current-buffer "*nrepl*"
+    (nrepl-send-string (format "(in-ns '%s)" ns) nrepl-buffer-ns (nrepl-handler (current-buffer)))))
 
 ;;; server
 (defun nrepl-server-filter (process output)
