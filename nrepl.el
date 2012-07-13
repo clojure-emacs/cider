6eb68bdcd48ff814254a36b2fd9d088e319229db
Tim King <kingtim@gmail.com>
2012-07-12 20:18:50 -0700

    bumped version

--- nrepl.el
+++ nrepl.el
@@ -5,7 +5,7 @@
 ;; Author: Tim King <kingtim@gmail.com>
 ;;         Phil Hagelberg <technomancy@gmail.com>
 ;; URL: http://www.github.com/kingtim/nrepl.el
-;; Version: 0.1.1
+;; Version: 0.1.2
 ;; Keywords: languages, clojure, nrepl
 ;; Package-Requires: ((clojure-mode "1.11"))
 
