26184a7775154bd78ce33c5ca9bc2503d30fa705
Tim King <kingtim@gmail.com>
2012-09-09 06:22:32 -0700

    Merge pull request #101 - Added a mention of melpa in nrepl.el as well

--- nrepl.el
+++ nrepl.el
@@ -30,10 +30,16 @@
 
 ;;; Installation:
 
-;; Available as a package in marmalade-repo.org.
+;; Available as a package in marmalade-repo.org and melpa.milkbox.net.
 
 ;; (add-to-list 'package-archives
 ;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;
+;; or
+;;
+;; (add-to-list 'package-archives
+;;              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;;
 ;; M-x package-install nrepl
 
 ;;; Usage:
