bb476a40ecf646583271c3803807c29eccff28e3
Hugo Duncan <hugo@hugoduncan.org>
2013-03-14 10:04:26 -0400

    Fix nrepl-connections-buffer-mode-map
    Define the mode-map before defining the mode, so the defvar actually
    gets initialised rather than having the mode define it as empty.

--- nrepl.el
+++ nrepl.el
@@ -1977,13 +1977,6 @@ Also closes associated repl and server buffers."
                         (get-buffer (nrepl-current-connection-buffer)))))
 
 ;;; Connection browser
-(define-derived-mode nrepl-connections-buffer-mode nrepl-popup-buffer-mode
-  "nREPL-Connections"
-  "nREPL Connections Buffer Mode.
-\\{nrepl-connections-buffer-mode-map}
-\\{nrepl-popup-buffer-mode-map}"
-  (set (make-local-variable 'truncate-lines) t))
-
 (defvar nrepl-connections-buffer-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map "d" 'nrepl-connections-make-default)
@@ -1991,6 +1984,13 @@ Also closes associated repl and server buffers."
     (define-key map (kbd "C-k") 'nrepl-connections-close-connection)
     (define-key map (kbd "RET") 'nrepl-connections-goto-connection)
     map))
+
+(define-derived-mode nrepl-connections-buffer-mode nrepl-popup-buffer-mode
+  "nREPL-Connections"
+  "nREPL Connections Buffer Mode.
+\\{nrepl-connections-buffer-mode-map}
+\\{nrepl-popup-buffer-mode-map}"
+  (set (make-local-variable 'truncate-lines) t))
 
 (defvar nrepl--connection-ewoc)
 (defconst nrepl--connection-browser-buffer-name "*nrepl-connections*")
