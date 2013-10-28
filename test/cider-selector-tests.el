d946841f3c96af79c06d8424e02884bef249b18e
Bozhidar Batsov <bozhidar@tradeo.com>
2013-10-28 14:47:24 +0200

    Extract cider-selector tests into a separate file

---
+++ test/cider-selector-tests.el
@@ -0,0 +1,57 @@
+(require 'cider)
+(require 'cider-selector)
+
+;; selector
+(defun cider-invoke-selector-method-by-key (ch)
+  (lexical-let ((method (find ch cider-selector-methods :key #'car)))
+        (funcall (third method))))
+
+(ert-deftest test-cider-selector-n ()
+  (with-temp-buffer
+    (lexical-let ((b1 (current-buffer)))
+      (set (make-local-variable 'cider-endpoint) '("123.123.123.123" 4006))
+      (let ((nrepl-connection-list (list (buffer-name b1))))
+        (nrepl-connection-browser)
+        (with-temp-buffer ;; switch to another buffer
+          (cider-invoke-selector-method-by-key ?n)
+          (should (equal (current-buffer)
+                         (get-buffer nrepl--connection-browser-buffer-name))))))))
+
+(ert-deftest test-cider-selector-c ()
+  (with-temp-buffer
+    (rename-buffer "*testfile*.clj")
+    (lexical-let ((b1 (current-buffer)))
+      (setq major-mode 'clojure-mode)
+      (with-temp-buffer
+        (rename-buffer "*testfile*.el")
+        (setq major-mode 'emacs-lisp-mode)
+        (with-temp-buffer
+          (should (not (equal (current-buffer) b1)))
+          (cider-invoke-selector-method-by-key ?e)
+          (should (not (equal (current-buffer) b1)))
+          (cider-invoke-selector-method-by-key ?c)
+          (should (equal (current-buffer) b1)))))))
+
+(ert-deftest test-cider-selector-e ()
+  (with-temp-buffer
+    (rename-buffer "*testfile*.el")
+    (lexical-let ((b1 (current-buffer)))
+      (setq major-mode 'emacs-lisp-mode)
+      (with-temp-buffer
+        (rename-buffer "*testfile*.clj")
+        (setq major-mode 'clojure-mode)
+        (with-temp-buffer
+          (should (not (equal (current-buffer) b1)))
+          (cider-invoke-selector-method-by-key ?c)
+          (should (not (equal (current-buffer) b1)))
+          (cider-invoke-selector-method-by-key ?e)
+          (should (equal (current-buffer) b1)))))))
+
+(ert-deftest test-cider-selector-v ()
+  (with-temp-buffer
+    (rename-buffer "*nrepl-events*")
+    (lexical-let ((b1 (current-buffer)))
+      (with-temp-buffer
+        (should (not (equal (current-buffer) b1)))
+        (cider-invoke-selector-method-by-key ?v)
+        (should (equal (current-buffer) b1))))))
