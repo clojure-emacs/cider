clojure-emacs/cider

commit 7a63d2bec3c8b765b7d86cf35c8530dafd6461b0

[Fix #552] Load resources from jar/zip files efficiently.
Instead of loading the entire `.jar` file into a buffer, just
extract the single resource to be displayed. Mark it read-only.
If that resource is already open, reuse it.

cider-interaction.el
@@ -532,18 +532,19 @@ Uses `find-file'."
         ((string-match "^\\(jar\\|zip\\):file:\\(.+\\)!/\\(.+\\)" resource)
          (let* ((jar (match-string 2 resource))
                 (path (match-string 3 resource))
-                (buffer-already-open (get-buffer (file-name-nondirectory jar))))
-           (cider-find-file jar)
-           (goto-char (point-min))
-           ;; Make sure the file path is followed by a newline to
-           ;; prevent eg. clj matching cljs.
-           (search-forward (concat path "\n"))
-           ;; moves up to matching line
-           (forward-line -1)
-           (let ((opened-buffer (current-buffer)))
-             (archive-extract)
-             (unless buffer-already-open
-               (kill-buffer opened-buffer)))))
+                (file (cider-emacs-or-clojure-side-adjustment jar))
+                (name (format "%s:%s" jar path)))
+           (switch-to-buffer
+            (or (get-file-buffer name)
+                (with-current-buffer (generate-new-buffer
+                                      (file-name-nondirectory path))
+                  (archive-zip-extract file path)
+                  (set-visited-file-name name)
+                  (setq-local default-directory (file-name-directory file))
+                  (setq-local buffer-read-only t)
+                  (set-buffer-modified-p nil)
+                  (set-auto-mode)
+                  (current-buffer))))))
         (t (error "Unknown resource path %s" resource))))
 
 (defun cider-jump-to-def-for (location)

commit d94c5cff3c5d43e3ff0f91413fac8b0918be3398

Fix cider-jump-to for tramp
Fix the opening of resources in jar files on tramp connections (broken by 7a63d2b).

cider-interaction.el
@@ -574,15 +574,29 @@ path or an entry within a zip/jar archive."
                       (path  (cider--file-path file))
                       (name  (format "%s:%s" path entry)))
            (or (get-file-buffer name)
-               (with-current-buffer (generate-new-buffer
-                                     (file-name-nondirectory entry))
-                 (archive-zip-extract path entry)
-                 (set-visited-file-name name)
-                 (setq-local default-directory (file-name-directory path))
-                 (setq-local buffer-read-only t)
-                 (set-buffer-modified-p nil)
-                 (set-auto-mode)
-                 (current-buffer)))))))
+               (if (tramp-tramp-file-p path)
+                   (progn
+                     ;; Use emacs built in archiving
+                     (find-file path)
+                     (goto-char (point-min))
+                     ;; Make sure the file path is followed by a newline to
+                     ;; prevent eg. clj matching cljs.
+                     (search-forward (concat entry "\n"))
+                     ;; moves up to matching line
+                     (forward-line -1)
+                     (let ((opened-buffer (current-buffer)))
+                       (archive-extract)
+                       (current-buffer)))
+                 ;; Use external zip program to just extract the single file
+                 (with-current-buffer (generate-new-buffer
+                                         (file-name-nondirectory entry))
+                     (archive-zip-extract path entry)
+                     (set-visited-file-name name)
+                     (setq-local default-directory (file-name-directory path))
+                     (setq-local buffer-read-only t)
+                     (set-buffer-modified-p nil)
+                     (set-auto-mode)
+                     (current-buffer))))))))
 
 (defun cider-find-var (var)
   "Return a buffer visiting the definition for VAR, or nil if not found."