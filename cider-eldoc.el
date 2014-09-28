720f22ecf6981457bb8678286a18b395977b49cb
Yves Zoundi <yveszoundi@gmail.com>
2014-09-28 18:32:14 -0400

    Improvements for pr-838.

--- cider-eldoc.el
+++ cider-eldoc.el
@@ -36,7 +36,8 @@
 (require 'eldoc)
 (require 'dash)
 
-(unless (fboundp 'eldoc-beginning-of-sexp)                                                                              
+(when (and (not (fboundp 'eldoc-beginning-of-sexp))
+           (fboundp 'elisp--beginning-of-sexp))
   (defalias 'eldoc-beginning-of-sexp 'elisp--beginning-of-sexp))
 
 (defvar cider-extra-eldoc-commands '("yas-expand")
