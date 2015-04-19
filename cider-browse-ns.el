38add97f7751c94db7819c523cf1fa5807c365cb
Bozhidar Batsov <bozhidar@batsov.com>
2015-04-19 22:02:10 +0300

    Code style

--- cider-browse-ns.el
+++ cider-browse-ns.el
@@ -37,7 +37,7 @@
 (require 'cider-client)
 (require 'cider-interaction)
 
-(defvar cider-browse-ns-buffer "*Browse NS*")
+(defconst cider-browse-ns-buffer "*Browse NS*")
 (defvar-local cider-browse-ns-current-ns nil)
 
 ;; Mode Definition
