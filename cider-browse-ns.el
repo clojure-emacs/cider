af17b0ab63448a9baf163f3522cf778452f90147
Oleh Krehel <ohwoeowho@gmail.com>
2015-06-25 12:35:22 +0200

    Change when-let to -when-let
    * cider-browse-ns.el (cider-browse-ns--find-at-point): Update.

--- cider-browse-ns.el
+++ cider-browse-ns.el
@@ -130,7 +130,7 @@
 
 (defun cider-browse-ns--find-at-point ()
   (interactive)
-  (when-let (var (cider-browse-ns--var-at-point))
+  (-when-let (var (cider-browse-ns--var-at-point))
     (cider-find-var current-prefix-arg var)))
 
 (defun cider-browse-ns--handle-mouse (event)
