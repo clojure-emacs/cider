b889e683285d87c84f598cf4b852a677fd1c25e3
Bozhidar Batsov <bozhidar@batsov.com>
2015-10-11 11:50:19 +0300

    Add missing "commentary" section

--- cider-common.el
+++ cider-common.el
@@ -17,6 +17,11 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; Common functions that are useful in both Clojure buffers and REPL
+;; buffers.
+
 ;;; Code:
 
 (require 'dash)
