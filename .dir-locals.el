a62cd0f103622cc4361c1b83b9d86599a2afab40
Artur Malabarba <bruce.connor.am@gmail.com>
2015-10-28 08:57:08 +0000

    Add .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,16 @@
-((nil . ((indent-tabs-mode . nil)
-         (fill-column . 80)
-         (sentence-end-double-space . t)
-         (emacs-lisp-docstring-fill-column . 75)
-         (checkdoc-arguments-in-order-flag . nil))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+;; The `nameless-*' variables normalize all files to have the same prefix with
+;; the `nameless' package, and instruct the package to not mess with
+;; indentation.
+
+((nil
+  (indent-tabs-mode)
+  (fill-column . 80)
+  (sentence-end-double-space . t)
+  (emacs-lisp-docstring-fill-column . 75)
+  (checkdoc-arguments-in-order-flag))
+ (emacs-lisp-mode
+  (nameless-affect-indentation-and-filling . nil)
+  (nameless-current-name . "cider")))
+
