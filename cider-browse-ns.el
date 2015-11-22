bf3f6ae105b459da02e955d8bab27e07df0de3f0
Bozhidar Batsov <bozhidar@batsov.com>
2015-11-22 11:00:39 +0200

    A few improvements to the front-matter in cider-browse-ns.el

--- cider-browse-ns.el
+++ cider-browse-ns.el
@@ -1,6 +1,6 @@
 ;;; cider-browse-ns.el --- CIDER namespace browser
 
-;; Copyright © 2014-2015 John Andrews
+;; Copyright © 2014-2015 John Andrews, Bozhidar Batsov
 
 ;; Author: John Andrews <john.m.andrews@gmail.com>
 
@@ -21,15 +21,17 @@
 
 ;;; Commentary:
 
-;; (cider-browse-ns)
+;; M-x cider-browse-ns
+;;
 ;; Display a list of all vars in a namespace.
 ;; Pressing <enter> will take you to the cider-doc buffer for that var.
-;; Pressing ^ will take you to a list of all namespaces (akin to dired mode)
+;; Pressing ^ will take you to a list of all namespaces (akin to `dired-mode').
 
-;; (cider-browse-ns-all)
-;; Explore clojure namespaces by browsing a list of all namespaces.
-;; Pressing enter expands into a list of that namespace's vars as if by
-;; executing the command (cider-browse-ns "my.ns")
+;; M-x cider-browse-ns-all
+;;
+;; Explore Clojure namespaces by browsing a list of all namespaces.
+;; Pressing <enter> expands into a list of that namespace's vars as if by
+;; executing the command (cider-browse-ns "my.ns").
 
 ;;; Code:
 
