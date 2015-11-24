af21a1c462111746afaff5da65e85f0c8b8af9b6
Bozhidar Batsov <bozhidar@batsov.com>
2015-11-24 09:12:37 +0200

    Use `cider-propertize-ns` wherever possible

--- cider-test.el
+++ cider-test.el
@@ -332,7 +332,7 @@ With the actual value, the outermost '(not ...)' s-expression is removed."
               'face (cond ((not (zerop error)) 'cider-test-error-face)
                           ((not (zerop fail))  'cider-test-failure-face)
                           (t                   'cider-test-success-face)))
-             (concat (propertize ns 'face 'font-lock-type-face)
+             (concat (cider-propertize-ns ns)
                      (propertize ": " 'face 'default))
              test fail error)))
 
@@ -434,14 +434,14 @@ This uses the Leiningen convention of appending '-test' to the namespace name."
 Upon test completion, results are echoed and a test report is optionally
 displayed. When test failures/errors occur, their sources are highlighted."
   (cider-test-clear-highlights)
-  (message "Running tests in %s..." (propertize ns 'face 'font-lock-type-face))
+  (message "Running tests in %s..." (cider-propertize-ns ns))
   (cider-nrepl-send-request
    (list "ns" ns "op" (if retest "retest" "test")
          "tests" tests "session" (cider-current-session))
    (lambda (response)
      (nrepl-dbind-response response (summary results status out err)
        (cond ((member "namespace-not-found" status)
-              (message "No tests namespace: %s" (propertize ns 'face 'font-lock-type-face)))
+              (message "No tests namespace: %s" (cider-propertize-ns ns)))
              (out (cider-emit-interactive-eval-output out))
              (err (cider-emit-interactive-eval-err-output err))
              (results
