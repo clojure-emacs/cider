7a9ed7503581f5d84ca98b5673070c4f332ae0e9
Bozhidar Batsov <bozhidar@batsov.com>
2016-01-01 19:44:29 +0000

    Add missing front-matter section to cider-popup.el

--- cider-popup.el
+++ cider-popup.el
@@ -17,6 +17,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; Common functionality for dealing with popup buffers.
+
 ;;; Code:
 
 (require 'nrepl-client)
