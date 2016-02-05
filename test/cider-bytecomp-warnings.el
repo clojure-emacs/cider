64391e5ff0fe04598ee68d1a5dac39e233b0e0b1
Bozhidar Batsov <bozhidar@batsov.com>
2016-02-05 18:51:42 +0200

    Fix a few checkdoc warnings

--- test/cider-bytecomp-warnings.el
+++ test/cider-bytecomp-warnings.el
@@ -1,6 +1,28 @@
-;; This is a script to be loaded while visiting a CIDER source file. It will
+;;; cider-bytecomp-warnings.el --- Check for byte-compilation problems
+
+;; Copyright © 2012-2016 Bozhidar Batsov and contributors
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This is a script to be loaded while visiting a CIDER source file.  It will
 ;; prepare all requirements and then byte-compile the file and signal an error
-;; on any warning. For example:
+;; on any warning.  For example:
+;;
 ;;    emacs -Q --batch -l test/cider-bytecomp-warnings.el cider-mode.el
 
 ;; This assumes that all CIDER dependencies are already on the package dir
@@ -14,3 +36,5 @@
 (load-file "cider-autoloads.el")
 (setq byte-compile-error-on-warn t)
 (batch-byte-compile)
+
+;;; cider-bytecomp-warnings.el ends here
