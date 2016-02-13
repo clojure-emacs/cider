9f5af95671b3628255d69380a943657027d1bc77
Bozhidar Batsov <bozhidar@batsov.com>
2016-02-13 11:13:22 +0200

    Backport string-trim and friends from subr-x

--- cider-compat.el
+++ cider-compat.el
@@ -153,5 +153,23 @@ to bind a single value, BINDINGS can just be a plain tuple."
       (declare (indent 1) (debug if-let))
       (list 'if-let bindings (macroexp-progn body)))))
 
+(eval-and-compile
+  (unless (fboundp 'string-trim)
+    (defsubst string-trim-left (string)
+      "Remove leading whitespace from STRING."
+      (if (string-match "\\`[ \t\n\r]+" string)
+          (replace-match "" t t string)
+        string))
+
+    (defsubst string-trim-right (string)
+      "Remove trailing whitespace from STRING."
+      (if (string-match "[ \t\n\r]+\\'" string)
+          (replace-match "" t t string)
+        string))
+
+    (defsubst string-trim (string)
+      "Remove leading and trailing whitespace from STRING."
+      (string-trim-left (string-trim-right string)))))
+
 (provide 'cider-compat)
 ;;; cider-compat.el ends here
