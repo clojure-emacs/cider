92cb518c9dc12837b705d111278a28bdc5c2f22a
Artur Malabarba <bruce.connor.am@gmail.com>
2016-03-06 14:47:31 -0300

    Prevent the debug prompt from inheriting faces

--- cider-debug.el
+++ cider-debug.el
@@ -216,11 +216,14 @@ Each element of LOCALS should be a list of at least two elements."
 
 (defun cider--debug-prompt (command-list)
   "Return prompt to display for COMMAND-LIST."
-  (concat
-   (mapconcat (lambda (x) (put-text-property 0 1 'face 'cider-debug-prompt-face x) x)
-              ;; `inspect' would conflict with `inject'.
-              (seq-difference command-list '("inspect")) " ")
-   "\n"))
+  ;; Force `default' face, otherwise the overlay "inherits" the face of the text
+  ;; after it.
+  (format (propertize "%s" 'face 'default)
+          (concat
+           (mapconcat (lambda (x) (put-text-property 0 1 'face 'cider-debug-prompt-face x) x)
+                      ;; `inspect' would conflict with `inject'.
+                      (seq-difference command-list '("inspect")) " ")
+           "\n")))
 
 (defvar-local cider--debug-prompt-overlay nil)
 
