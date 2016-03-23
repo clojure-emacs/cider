09e253f497baf0354175d57fcc635c32e1330e2e
Artur Malabarba <bruce.connor.am@gmail.com>
2016-03-23 13:27:42 -0300

    Extend copyright in cider-debug and cider-overlays
    They were originally generated by `auto-insert`

--- cider-debug.el
+++ cider-debug.el
@@ -1,6 +1,6 @@
 ;;; cider-debug.el --- CIDER interaction with the cider.debug nREPL middleware  -*- lexical-binding: t; -*-
 
-;; Copyright © 2015-2016 Artur Malabarba
+;; Copyright © 2015-2016 Bozhidar Batsov, Artur Malabarba and CIDER contributors
 
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>
 
--- cider-overlays.el
+++ cider-overlays.el
@@ -1,6 +1,6 @@
 ;;; cider-overlays.el --- Managing CIDER overlays  -*- lexical-binding: t; -*-
 
-;; Copyright © 2015-2016 Artur Malabarba
+;; Copyright © 2015-2016 Bozhidar Batsov, Artur Malabarba and CIDER contributors
 
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>
 
