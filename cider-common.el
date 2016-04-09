3bdbd1029537599e66ee1a248464167884beefa6
Bozhidar Batsov <bozhidar@batsov.com>
2016-04-09 07:49:42 -0700

    Fix some copyright symbols

--- cider-common.el
+++ cider-common.el
@@ -1,6 +1,6 @@
 ;;; cider-common.el --- Common use functions         -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015-2016  Artur Malabarba
+;; Copyright © 2015-2016  Artur Malabarba
 
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>
 
--- test/cider-overlay-tests.el
+++ test/cider-overlay-tests.el
@@ -1,6 +1,6 @@
 ;;; cider-overlay-tests.el                       -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015-2016 Bozhidar Batsov, Artur Malabarba and CIDER contributors
+;; Copyright © 2015-2016 Bozhidar Batsov, Artur Malabarba and CIDER contributors
 
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>
 
