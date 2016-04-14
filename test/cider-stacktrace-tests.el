fc8cd505368448d8843497cce330177d92ab1215
cskksc <ckoparkar@live.in>
2016-04-14 22:17:33 +0530

    [#1650] Migrate some tests to buttercup
    Tests migrated:
    - cider-client-tests.el
    - cider-overlay-tests.el
    - cider-stacktrace-tests.el
    - cider-util-tests.el

--- test/cider-stacktrace-tests.el
+++ test/cider-stacktrace-tests.el
@@ -1,33 +1,56 @@
+(require 'buttercup)
 (require 'cider)
 (require 'cider-stacktrace)
-(require 'ert)
 
 ;;; cider-stacktrace tests
 
 ;;; Internal/Middleware error suppression
-(ert-deftest test-cider-stacktrace-some-suppressed-errors-p ()
-  (let ((cider-stacktrace-suppressed-errors '()))
-    (should-not (cider-stacktrace-some-suppressed-errors-p '("a")))
-    (should-not (cider-stacktrace-some-suppressed-errors-p '())))
+(describe "cider-stacktrace-some-suppressed-errors-p"
+  :var (cider-stacktrace-suppressed-errors)
 
-  (let ((cider-stacktrace-suppressed-errors '("a" "b" "c" "d")))
-    (should (equal '("a") (cider-stacktrace-some-suppressed-errors-p '("a"))))
-    (should (equal '("a" "c") (cider-stacktrace-some-suppressed-errors-p '("a" "c" "e"))))
-    (should-not (cider-stacktrace-some-suppressed-errors-p '("g" "f" "e")))))
+  (describe "when no errors are suppressed"
+    (it "returns nil"
+      (setq cider-stacktrace-suppressed-errors '())
+      (expect (cider-stacktrace-some-suppressed-errors-p '("a"))
+              :to-equal nil)
+      (expect (cider-stacktrace-some-suppressed-errors-p '())
+              :to-equal nil)))
 
-(ert-deftest test-cider-stacktrace-suppress-error ()
-  (let ((cider-stacktrace-suppressed-errors '("a" "b" "c")))
-    (should-not (cl-set-exclusive-or '("a" "b" "z" "c") (cider-stacktrace-suppress-error "z") :test 'equal))))
+  (describe "when some errors are suppressed"
+    (it "returns a list of suppressed errors and all errors associated with them"
+      (setq cider-stacktrace-suppressed-errors '("a" "b" "c" "d"))
+      (expect (cider-stacktrace-some-suppressed-errors-p '("a"))
+              :to-equal '("a"))
+      (expect (cider-stacktrace-some-suppressed-errors-p '("a" "c" "e"))
+              :to-equal '("a" "c")))))
 
-(ert-deftest test-cider-stacktrace-promote-error ()
-  (let ((cider-stacktrace-suppressed-errors '("a" "b" "x" "c")))
-    (should-not (cl-set-exclusive-or '("a" "b" "c") (cider-stacktrace-promote-error "x")
-                                     :test 'equal))))
+(describe "cider-stacktrace-suppressed-error-p"
+  :var (cider-stacktrace-suppressed-errors)
 
-(ert-deftest test-cider-stacktrace-suppressed-error-p ()
-  (let ((cider-stacktrace-suppressed-errors '("a" "b" "g" "j")))
-    (should (cider-stacktrace-suppressed-error-p "a"))
-    (should (cider-stacktrace-suppressed-error-p "b"))
-    (should (cider-stacktrace-suppressed-error-p "g"))
-    (should (cider-stacktrace-suppressed-error-p "j"))
-    (should-not (cider-stacktrace-suppressed-error-p "c"))))
+  (it "returns true when a error is suppressed"
+    (setq cider-stacktrace-suppressed-errors '("a" "b" "g" "j"))
+    (expect (cider-stacktrace-suppressed-error-p "a") :to-be-truthy)
+    (expect (cider-stacktrace-suppressed-error-p "b") :to-be-truthy)
+    (expect (cider-stacktrace-suppressed-error-p "g") :to-be-truthy)
+    (expect (cider-stacktrace-suppressed-error-p "j") :to-be-truthy)
+    (expect (cider-stacktrace-suppressed-error-p "c") :not :to-be-truthy)))
+
+(describe "cider-stacktrace-suppress-error"
+  :var (cider-stacktrace-suppressed-errors)
+
+  (it "adds the error to the suppressed errors list"
+    (setq cider-stacktrace-suppressed-errors '("a" "b" "c"))
+    (expect (cl-set-exclusive-or '("a" "b" "z" "c")
+                                 (cider-stacktrace-suppress-error "z")
+                                 :test 'equal)
+            :not :to-be-truthy)))
+
+(describe "cider-stacktrace-promote-error"
+  :var (cider-stacktrace-suppressed-errors)
+
+  (it "removes the error from the suppressed errors list"
+    (setq cider-stacktrace-suppressed-errors '("a" "b" "x" "c"))
+    (expect (cl-set-exclusive-or '("a" "b" "c")
+                                 (cider-stacktrace-promote-error "x")
+                                 :test 'equal)
+            :not :to-be-truthy)))
--- test/cider-util-tests.el
+++ test/cider-util-tests.el
@@ -1,31 +1,35 @@
+(require 'buttercup)
 (require 'cider)
 (require 'cider-util)
-(require 'ert)
-(require 'noflet)
 
 ;;; cider-util tests
-(ert-deftest cider-symbol-at-point-dont-move ()
-  (with-temp-buffer
-    (clojure-mode)
-    (insert "something else\n")
-    (should (not (cider-symbol-at-point)))
-    (should (equal "else" (cider-symbol-at-point 'lookback)))
-    (should (= (point) (point-max)))))
+(describe "cider-symbol-at-point"
+  (it "doesn't move the cursor"
+    (with-temp-buffer
+      (clojure-mode)
+      (insert "something else\n")
+      (expect (cider-symbol-at-point) :not :to-be-truthy)
+      (expect (cider-symbol-at-point 'lookback) :to-equal "else")
+      (expect (point) :to-equal (point-max)))))
 
-(ert-deftest cider--version-fallback ()
-  (noflet ((pkg-info-version-info (package) (error "No version")))
-    (let ((cider-version "0.11.0")
-          (cider-codename "Victory"))
-      (should (string= (cider--version) "0.11.0 (Victory)")))))
 
-(ert-deftest cider--version-stable-version ()
-  (noflet ((pkg-info-version-info (package) "0.11.0"))
-    (let ((cider-version "0.11.0")
-          (cider-codename "Victory"))
-      (should (string= (cider--version) "0.11.0 (Victory)")))))
+(describe "cider--version"
+  :var (cider-version cider-codename)
 
-(ert-deftest cider--version-snapshot-version ()
-  (noflet ((pkg-info-version-info (package) "0.11.0snapshot (package: 20160301.2217)"))
-    (let ((cider-version "0.11.0-snapshot")
-          (cider-codename "Victory"))
-      (should (string= (cider--version) "0.11.0snapshot (package: 20160301.2217)")))))
+  (it "handles version unavailable error"
+    (spy-on 'pkg-info-version-info :and-throw-error '(error "No version"))
+    (setq cider-version "0.11.0"
+          cider-codename "Victory")
+    (expect (cider--version) :to-equal "0.11.0 (Victory)"))
+
+  (it "returns correct version number when available"
+    (spy-on 'pkg-info-version-info :and-return-value "0.11.0")
+    (setq cider-version "0.11.0"
+          cider-codename "Victory")
+    (expect (cider--version) :to-equal "0.11.0 (Victory)"))
+
+  (it "handles snapshot versions"
+    (spy-on 'pkg-info-version-info :and-return-value "0.11.0snapshot (package: 20160301.2217)")
+    (setq cider-version "0.11.0-snapshot"
+          cider-codename "Victory")
+    (expect (cider--version) :to-equal "0.11.0snapshot (package: 20160301.2217)")))
