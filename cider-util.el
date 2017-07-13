fa9f1cbd3d5be7eaafcdd038267f6466d651def5
Paul Landes <landes@mailc.net>
2017-07-13 17:07:47 -0500

    strip properties from puget output to make consistent with clojure-mode fontification

--- cider-util.el
+++ cider-util.el
@@ -258,7 +258,7 @@ This buffer is not designed to display anything to the user.  For that, use
 (defun cider-font-lock-as (mode string)
   "Use MODE to font-lock the STRING."
   (let ((string (if (cider-ansi-color-string-p string)
-                    (ansi-color-apply string)
+                    (substring-no-properties (ansi-color-apply string))
                   string)))
     (if (or (null cider-font-lock-max-length)
             (< (length string) cider-font-lock-max-length))
