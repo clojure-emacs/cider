f99a2c552ca3589c01a07e5cf37d0238450e32c5
Tianxiang Xiong <tianxiang.xiong@gmail.com>
2017-12-12 19:19:37 -0800

    Remove single tuple bindings for `{if, when}-let*`
    In Emacs 26, `{if, when}-let*` do not allow single tuple bindings.

--- cider-compat.el
+++ cider-compat.el
@@ -130,14 +130,9 @@ threading."
       "Process BINDINGS and if all values are non-nil eval THEN, else ELSE.
 Argument BINDINGS is a list of tuples whose car is a symbol to be
 bound and (optionally) used in THEN, and its cadr is a sexp to be
-evalled to set symbol's value.  In the special case you only want
-to bind a single value, BINDINGS can just be a plain tuple."
+evalled to set symbol's value."
       (declare (indent 2)
                (debug ([&or (&rest (symbolp form)) (symbolp form)] form body)))
-      (when (and (<= (length bindings) 2)
-                 (not (listp (car bindings))))
-        ;; Adjust the single binding case
-        (setq bindings (list bindings)))
       `(let* ,(internal--build-bindings bindings)
          (if ,(car (internal--listify (car (last bindings))))
              ,then
@@ -148,8 +143,7 @@ to bind a single value, BINDINGS can just be a plain tuple."
       "Process BINDINGS and if all values are non-nil eval BODY.
 Argument BINDINGS is a list of tuples whose car is a symbol to be
 bound and (optionally) used in BODY, and its cadr is a sexp to be
-evalled to set symbol's value.  In the special case you only want
-to bind a single value, BINDINGS can just be a plain tuple."
+evalled to set symbol's value."
       (declare (indent 1) (debug if-let*))
       `(if-let* ,bindings ,(macroexp-progn body)))))
 
