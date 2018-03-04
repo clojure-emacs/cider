7a6ad8fdaa2e7ba3ff6be69a234a785e777f26c6
Gregory Ganley <gganley@student.bridgew.edu>
2018-03-03 20:37:56 -0500

    [Fix #2211] Scope .dir-locals.el settings to specific modes (#2221)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
+((emacs-lisp-mode
   (bug-reference-url-format . "https://github.com/clojure-emacs/cider/issues/%s")
   (indent-tabs-mode)
   (fill-column . 80)
@@ -9,8 +9,7 @@
   (emacs-lisp-docstring-fill-column . 75)
   (checkdoc-symbol-words . ("top-level" "major-mode" "macroexpand-all" "print-level" "print-length"))
   (checkdoc-package-keywords-flag)
-  (checkdoc-arguments-in-order-flag))
- (emacs-lisp-mode
+  (checkdoc-arguments-in-order-flag)
   (bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)")))
 
 ;; To use the bug-reference stuff, do:
