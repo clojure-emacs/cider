f9bd359af587aa402c10e1bdb328dfc91fa065d3
Radon Rosborough <radon.neon@gmail.com>
2018-03-14 15:23:08 -0700

    Allow predicates in plugin and middleware lists
    See https://github.com/clojure-emacs/clj-refactor.el/pull/392#issuecomment-352399191

--- test/cider-tests.el
+++ test/cider-tests.el
@@ -134,7 +134,62 @@
                 :to-equal "-C -o -i \"(require 'cider.tasks)\" -d org.clojure/tools.nrepl\\:0.2.12 -d cider/cider-nrepl\\:0.11.0 cider.tasks/add-middleware -m cider.nrepl/cider-middleware repl -s wait"))
     (it "can concat in a gradle project"
         (expect (cider-inject-jack-in-dependencies "-m" "--no-daemon clojureRepl" "gradle")
-                :to-equal "-m --no-daemon clojureRepl"))))
+                :to-equal "-m --no-daemon clojureRepl")))
+
+  (describe "when there are predicates"
+    :var (plugins-predicate middlewares-predicate)
+
+    (before-each
+      (fset 'plugins-predicate (lambda (&rest _) t))
+      (fset 'middlewares-predicate (lambda (&rest _) t))
+      (setq-local cider-jack-in-lein-plugins '(("refactor-nrepl" "2.0.0" :predicate plugins-predicate) ("cider/cider-nrepl" "0.11.0")))
+      (setq-local cider-jack-in-nrepl-middlewares '(("refactor-nrepl.middleware/wrap-refactor" :predicate middlewares-predicate) "cider.nrepl/cider-middleware" ("another/middleware"))))
+    (it "includes plugins whose predicates return true"
+      (expect (cider-jack-in-normalized-lein-plugins)
+              :to-equal '(("refactor-nrepl" "2.0.0") ("cider/cider-nrepl" "0.11.0"))))
+    (it "includes middlewares whose predicates return true"
+      (expect (cider-jack-in-normalized-nrepl-middlewares)
+              :to-equal '("refactor-nrepl.middleware/wrap-refactor" "cider.nrepl/cider-middleware" "another/middleware")))
+    (it "ignores plugins whose predicates return false"
+      (spy-on 'plugins-predicate :and-return-value nil)
+      (expect (cider-jack-in-normalized-lein-plugins)
+              :to-equal '(("cider/cider-nrepl" "0.11.0"))))
+    (it "ignores plugins whose predicates return false"
+      (spy-on 'middlewares-predicate :and-return-value nil)
+      (expect (cider-jack-in-normalized-nrepl-middlewares)
+              :to-equal '("cider.nrepl/cider-middleware" "another/middleware")))
+    (it "calls plugin predicates with the whole list entry"
+      (spy-on 'plugins-predicate)
+      (cider-jack-in-normalized-lein-plugins)
+      (expect 'plugins-predicate
+              :to-have-been-called-with '("refactor-nrepl" "2.0.0" :predicate plugins-predicate)))
+    (it "calls middleware predicates with the whole list entry"
+      (spy-on 'middlewares-predicate)
+      (cider-jack-in-normalized-nrepl-middlewares)
+      (expect 'middlewares-predicate
+              :to-have-been-called-with '("refactor-nrepl.middleware/wrap-refactor" :predicate middlewares-predicate)))
+    (it "only calls plugin predicates for their own entries"
+      (spy-on 'plugins-predicate)
+      (cider-jack-in-normalized-lein-plugins)
+      (expect 'plugins-predicate :to-have-been-called-times 1))
+    (it "only calls middleware predicates for their own entries"
+      (spy-on 'middlewares-predicate)
+      (cider-jack-in-normalized-nrepl-middlewares)
+      (expect 'middlewares-predicate :to-have-been-called-times 1)))
+
+  (describe "when the middleware and plugin lists have been normalized"
+    (before-each
+      (spy-on 'cider-jack-in-normalized-nrepl-middlewares
+              :and-return-value '("refactor-nrepl.middleware/wrap-refactor" "cider.nrepl/cider-middleware"))
+      (spy-on 'cider-jack-in-normalized-lein-plugins
+              :and-return-value '(("refactor-nrepl" "2.0.0") ("cider/cider-nrepl" "0.11.0")))
+      (setq-local cider-jack-in-dependencies-exclusions '()))
+    (it "uses them in a lein project"
+      (expect (cider-inject-jack-in-dependencies "" "repl :headless" "lein")
+              :to-equal "update-in :dependencies conj \\[org.clojure/tools.nrepl\\ \\\"0.2.12\\\"\\] -- update-in :plugins conj \\[refactor-nrepl\\ \\\"2.0.0\\\"\\] -- update-in :plugins conj \\[cider/cider-nrepl\\ \\\"0.11.0\\\"\\] -- repl :headless"))
+    (it "uses them in a boot project"
+      (expect (cider-inject-jack-in-dependencies "" "repl -s wait" "boot")
+              :to-equal "-i \"(require 'cider.tasks)\" -d org.clojure/tools.nrepl\\:0.2.12 -d refactor-nrepl\\:2.0.0 -d cider/cider-nrepl\\:0.11.0 cider.tasks/add-middleware -m refactor-nrepl.middleware/wrap-refactor -m cider.nrepl/cider-middleware repl -s wait"))))
 
 (describe "cider-jack-in-auto-inject-clojure"
   (it "injects `cider-minimum-clojure-version' when `cider-jack-in-auto-inject-clojure' is set to minimal"
