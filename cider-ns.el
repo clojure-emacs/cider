8b8309d9702a98dbfb1804de3d643b36b188057b
Bozhidar Batsov <bozhidar@batsov.com>
2018-07-10 10:05:49 +0300

    Add some version info the deprecation messages

--- cider-ns.el
+++ cider-ns.el
@@ -44,7 +44,7 @@ If t, save the files without confirmation."
   :group 'cider
   :package-version '(cider . "0.15.0"))
 
-(define-obsolete-variable-alias 'cider-save-files-on-cider-ns-refresh 'cider-ns-save-files-on-refresh)
+(define-obsolete-variable-alias 'cider-save-files-on-cider-ns-refresh 'cider-ns-save-files-on-refresh "0.18")
 
 (defconst cider-ns-refresh-log-buffer "*cider-ns-refresh-log*")
 
@@ -70,7 +70,7 @@ prevent reloading from occurring."
   :group 'cider
   :package-version '(cider . "0.10.0"))
 
-(define-obsolete-variable-alias 'cider-refresh-before-fn 'cider-ns-refresh-before-fn)
+(define-obsolete-variable-alias 'cider-refresh-before-fn 'cider-ns-refresh-before-fn "0.18")
 
 (defcustom cider-ns-refresh-after-fn nil
   "Clojure function for `cider-ns-refresh' to call after reloading.
@@ -80,7 +80,7 @@ namespace-qualified function of zero arity."
   :group 'cider
   :package-version '(cider . "0.10.0"))
 
-(define-obsolete-variable-alias 'cider-refresh-after-fn 'cider-ns-refresh-after-fn)
+(define-obsolete-variable-alias 'cider-refresh-after-fn 'cider-ns-refresh-after-fn "0.18")
 
 (defun cider-ns-refresh--handle-response (response log-buffer)
   "Refresh LOG-BUFFER with RESPONSE."
@@ -200,6 +200,8 @@ refresh functions (defined in `cider-ns-refresh-before-fn' and
            (lambda (response)
              (cider-ns-refresh--handle-response response log-buffer))
            conn))))))
+
+(define-obsolete-function-alias 'cider-refresh 'cider-ns-refresh "0.18")
 
 (provide 'cider-ns)
 ;;; cider-ns.el ends here
