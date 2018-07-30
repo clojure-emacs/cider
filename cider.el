3a725f3ae14d2d88efa52631f978b5979f635ce6
Vitalie Spinu <spinuvit@gmail.com>
2018-07-30 13:44:50 +0200

    [Fix #2381] Start REPLs in the original jack-in buffer

--- cider.el
+++ cider.el
@@ -901,6 +901,7 @@ prompt for all these parameters."
   (let ((cider-jack-in-dependencies (append cider-jack-in-dependencies cider-jack-in-cljs-dependencies))
         (cider-jack-in-lein-plugins (append cider-jack-in-lein-plugins cider-jack-in-cljs-lein-plugins))
         (cider-jack-in-nrepl-middlewares (append cider-jack-in-nrepl-middlewares cider-jack-in-cljs-nrepl-middlewares))
+        (orig-buffer (current-buffer))
         (params (thread-first params
                   (cider--update-project-dir)
                   (cider--update-jack-in-cmd))))
@@ -908,7 +909,8 @@ prompt for all these parameters."
      (plist-get params :project-dir)
      (plist-get params :jack-in-cmd)
      (lambda (server-buffer)
-       (cider-connect-sibling-cljs params server-buffer)))))
+       (with-current-buffer orig-buffer
+         (cider-connect-sibling-cljs params server-buffer))))))
 
 ;;;###autoload
 (defun cider-jack-in-clj&cljs (&optional params soft-cljs-start)
@@ -921,6 +923,7 @@ cljs REPL only when the ClojureScript dependencies are met."
   (let ((cider-jack-in-dependencies (append cider-jack-in-dependencies cider-jack-in-cljs-dependencies))
         (cider-jack-in-lein-plugins (append cider-jack-in-lein-plugins cider-jack-in-cljs-lein-plugins))
         (cider-jack-in-nrepl-middlewares (append cider-jack-in-nrepl-middlewares cider-jack-in-cljs-nrepl-middlewares))
+        (orig-buffer (current-buffer))
         (params (thread-first params
                   (cider--update-project-dir)
                   (cider--update-jack-in-cmd)
@@ -930,11 +933,12 @@ cljs REPL only when the ClojureScript dependencies are met."
      (plist-get params :project-dir)
      (plist-get params :jack-in-cmd)
      (lambda (server-buffer)
-       (let ((clj-repl (cider-connect-sibling-clj params server-buffer)))
-         (if soft-cljs-start
-             (when (cider--check-cljs (plist-get params :cljs-repl-type) 'no-error)
-               (cider-connect-sibling-cljs params clj-repl))
-           (cider-connect-sibling-cljs params clj-repl)))))))
+       (with-current-buffer orig-buffer
+         (let ((clj-repl (cider-connect-sibling-clj params server-buffer)))
+           (if soft-cljs-start
+               (when (cider--check-cljs (plist-get params :cljs-repl-type) 'no-error)
+                 (cider-connect-sibling-cljs params clj-repl))
+             (cider-connect-sibling-cljs params clj-repl))))))))
 
 ;;;###autoload
 (defun cider-connect-sibling-clj (params &optional other-repl)
