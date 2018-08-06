d3ed8596b7658da06aabd05a5c32c213bb253453
Bozhidar Batsov <bozhidar@batsov.com>
2018-08-06 22:13:24 +0300

    Mention cider-load-buffer-and-switch-to-repl-buffer in cider-mode's menu

--- cider-mode.el
+++ cider-mode.el
@@ -348,6 +348,7 @@ If invoked with a prefix ARG eval the expression after inserting it."
     ["Insert ns form in REPL" cider-insert-ns-form-in-repl]
     "--"
     ["Load this buffer" cider-load-buffer]
+    ["Load this buffer and switch to REPL" cider-load-buffer-and-switch-to-repl-buffer]
     ["Load another file" cider-load-file]
     ["Recursively load all files in directory" cider-load-all-files]
     ["Load all project files" cider-load-all-project-ns]
