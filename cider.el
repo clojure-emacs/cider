c8bd06faf66f182550e91f558a653dc5e1a18a1c
Bozhidar Batsov <bozhidar@batsov.com>
2019-06-07 23:13:17 +0300

    Bump the required cider-nrepl

--- cider.el
+++ cider.el
@@ -399,7 +399,7 @@ Elements of the list are artifact name and list of exclusions to apply for the a
 (defconst cider-latest-clojure-version "1.10.0"
   "Latest supported version of Clojure.")
 
-(defconst cider-required-middleware-version "0.22.0-beta3"
+(defconst cider-required-middleware-version "0.22.0-beta4"
   "The CIDER nREPL version that's known to work properly with CIDER.")
 
 (defcustom cider-jack-in-auto-inject-clojure nil
