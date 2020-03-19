370f9c6c87c08f41bbbedb68716dbe9c9c004cb0
yuhan0 <qythium@gmail.com>
2020-03-20 00:51:40 +0800

    Add org-table declarations for byte compiler

--- cider-doc.el
+++ cider-doc.el
@@ -37,6 +37,11 @@
 (require 'easymenu)
 (require 'cider-browse-spec)
 
+(declare-function org-table-map-tables 'org-table)
+(declare-function org-table-align 'org-table)
+(declare-function org-table-begin 'org-table)
+(declare-function org-table-end 'org-table)
+
 
 ;;; Variables
 
