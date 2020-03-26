c9ec42daeffaeb1980b8ab27e8ce782109fcdf7c
yuhan0 <qythium@gmail.com>
2020-03-26 22:24:19 +0800

    Trim ' prefixes from cider-symbol-at-point
    Following changes in clojure-mode syntax table where ' is now a symbol
    constituent

--- cider-util.el
+++ cider-util.el
@@ -129,8 +129,8 @@ Ignores the REPL prompt.  If LOOK-BACK is non-nil, move backwards trying to
 find a symbol if there isn't one at point."
   (or (when-let* ((str (thing-at-point 'symbol)))
         (unless (text-property-any 0 (length str) 'field 'cider-repl-prompt str)
-          ;; Remove font-locking and trailing . from constructors like Record.
-          (string-remove-suffix "." (substring-no-properties str))))
+          ;; Remove font-locking, prefix quotes, and trailing . from constructors like Record.
+          (string-remove-prefix "'" (string-remove-suffix "." (substring-no-properties str)))))
       (when look-back
         (save-excursion
           (ignore-errors
