94d81655af8ff46218d07342e044d02bbacd2b97
Bozhidar Batsov <bozhidar@batsov.com>
2020-09-13 14:35:46 +0300

    Tweak the package commentary

--- cider.el
+++ cider.el
@@ -33,7 +33,7 @@
 ;;; Commentary:
 
 ;; Provides a Clojure interactive development environment for Emacs, built on
-;; top of nREPL.
+;; top of nREPL.  See https://docs.cider.mx for more details.
 
 ;;; Installation:
 
