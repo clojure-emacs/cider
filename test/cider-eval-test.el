28fac9cc46fb2387c9afb46d99e1c9b264690664
Arne Brasseur <arne@arnebrasseur.net>
2021-08-31 10:40:01 +0200

    Sideloader: no line breaks in base64 content
    Currently we are sending sideloaded resources as line-wrapped base64. The extra
    newlines confuse the nREPL sideloader, turning the result into binary garbage.
    
    I think in this case we should be both more conservative in what we send (this
    patch) and more lenient in what we receive (nREPL should sanitize the input and
    drop any characters that aren't valid base64).

---
+++ test/cider-eval-test.el
@@ -0,0 +1,42 @@
+;;; cider-eval-tests.el
+
+;; Copyright © 2012-2021 Arne Brasseur
+
+;; Author: Arne Brasseur
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see `http://www.gnu.org/licenses/'.
+
+;;; Commentary:
+
+;; This file is part of CIDER
+
+;;; Code:
+
+(require 'buttercup)
+(require 'cider-eval)
+
+(describe "cider-provide-file"
+  (it "returns an empty string when the file is not found"
+    (expect (cider-provide-file "abc.clj") :to-equal ""))
+  (it "base64 encodes without newlines"
+    (let ((cider-sideloader-dir "/tmp")
+          (default-directory "/tmp")
+          (filename (make-temp-file "abc.clj")))
+      (with-temp-file filename
+        (dotimes (_ 60) (insert "x")))
+      (expect (cider-provide-file filename) :not :to-match "\n"))))
+
+(provide 'cider-eval-tests)
