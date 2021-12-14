1bf696f20561293cb83c1f580f686e71dc2a507a
vemv <vemv@users.noreply.github.com>
2021-12-14 07:43:53 +0100

    Bump enrich-classpath

--- cider.el
+++ cider.el
@@ -519,7 +519,7 @@ where it doesn't make sense."
   (let* ((corpus (if (and cider-enrich-classpath
                           (eq project-type 'lein))
                      (append cider-jack-in-lein-plugins
-                             '(("mx.cider/enrich-classpath" "1.4.1")))
+                             '(("mx.cider/enrich-classpath" "1.5.0")))
                    cider-jack-in-lein-plugins)))
     (thread-last corpus
       (seq-filter
