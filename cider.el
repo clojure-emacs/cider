29f192809c5f02be4708ef804ab2a0ffc7284574
vemv <vemv@users.noreply.github.com>
2021-12-21 20:25:25 +0100

    Bump enrich-classpath
    Delivers https://github.com/clojure-emacs/enrich-classpath/commit/b85556605590f9b334238c5704b03f0f31186375 which makes sure e.g. `lein-doo` won't pull an overly old version of Clojure, which would make Fipp fail.

--- cider.el
+++ cider.el
@@ -519,7 +519,7 @@ where it doesn't make sense."
   (let* ((corpus (if (and cider-enrich-classpath
                           (eq project-type 'lein))
                      (append cider-jack-in-lein-plugins
-                             '(("mx.cider/enrich-classpath" "1.5.0")))
+                             '(("mx.cider/enrich-classpath" "1.5.1")))
                    cider-jack-in-lein-plugins)))
     (thread-last corpus
       (seq-filter
