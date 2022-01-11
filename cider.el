339d980c3eb14aff1ec1f6ea1f612e0baa3253fe
Bozhidar Batsov <bozhidar@batsov.dev>
2022-01-11 10:25:34 +0200

    Bump the injected cider-nrepl

--- cider.el
+++ cider.el
@@ -418,7 +418,7 @@ the artifact.")
 (defconst cider-latest-clojure-version "1.10.1"
   "Latest supported version of Clojure.")
 
-(defconst cider-required-middleware-version "0.27.4"
+(defconst cider-required-middleware-version "0.28.0"
   "The CIDER nREPL version that's known to work properly with CIDER.")
 
 (defcustom cider-injected-middleware-version cider-required-middleware-version
