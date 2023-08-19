d4cfe220bf736c804fc5017205f4a70d2163a00c
vemv <vemv@users.noreply.github.com>
2023-08-19 07:57:17 +0200

    Bump enrich-classpath

--- cider.el
+++ cider.el
@@ -588,7 +588,7 @@ returned by this function does not include keyword arguments."
   (let ((plugins (if cider-enrich-classpath
                      (append cider-jack-in-lein-plugins
                              `(("cider/cider-nrepl" ,cider-injected-middleware-version)
-                               ("mx.cider/lein-enrich-classpath" "1.13.0")))
+                               ("mx.cider/lein-enrich-classpath" "1.14.0")))
                    (append cider-jack-in-lein-plugins
                            `(("cider/cider-nrepl" ,cider-injected-middleware-version))))))
     (thread-last
