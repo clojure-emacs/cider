6b36f091069955a43d3d25ed5b681735af9528ae
vemv <vemv@users.noreply.github.com>
2023-09-27 14:18:41 +0200

    Use enrich-classpath 1.18.0 (#3486)
    https://github.com/clojure-emacs/enrich-classpath/compare/v1.17.2...v1.18.0
    https://clojars.org/mx.cider/enrich-classpath/versions/1.18.0

--- cider.el
+++ cider.el
@@ -598,7 +598,7 @@ returned by this function does not include keyword arguments."
   (let ((plugins (if cider-enrich-classpath
                      (append cider-jack-in-lein-plugins
                              `(("cider/cider-nrepl" ,cider-injected-middleware-version)
-                               ("mx.cider/lein-enrich-classpath" "1.17.2")))
+                               ("mx.cider/lein-enrich-classpath" "1.18.0")))
                    (append cider-jack-in-lein-plugins
                            `(("cider/cider-nrepl" ,cider-injected-middleware-version))))))
     (thread-last
