clojure-emacs/cider

commit 47d9a95c085c812747e9a95229495cd0638bba57

Make `cider-repl-set-ns` more reliable on Piggieback connections
Fixes #3503
Partially reverts 5f705b

cider-repl.el
@@ -1321,8 +1321,15 @@ command will prompt for the name of the namespace to switch to."
       ;; That's why we eval them separately instead of combining them with `do'.
       (when cider-repl-require-ns-on-set
         (cider-sync-tooling-eval (format "(require '%s)" ns) nil connection))
-      (cider-tooling-eval (format "(in-ns '%s)" ns)
-                          (cider-repl-switch-ns-handler connection)))))
+      (let ((f (if (equal 'cljs
+                          (with-current-buffer connection
+                            cider-repl-type))
+                   ;; For cljs, don't use cider-sync-tooling-eval, because Piggieback will later change the ns (issue #3503):
+                   #'cider-nrepl-request:eval
+                 ;; When possible, favor cider-sync-tooling-eval because it preserves *1, etc (commit 5f705b):
+                 #'cider-sync-tooling-eval)))
+        (funcall f (format "(in-ns '%s)" ns)
+                 (cider-repl-switch-ns-handler connection))))))
 
 
 ;;; Location References

commit 27c018cb488e35304b54425fb37d7c9083b806c2

Use the right eval function
Fixes a bug introduced in https://github.com/clojure-emacs/cider/commit/47d9a95c085c812747e9a95229495cd0638bba57

cider-repl.el
@@ -1324,10 +1324,10 @@ command will prompt for the name of the namespace to switch to."
       (let ((f (if (equal 'cljs
                           (with-current-buffer connection
                             cider-repl-type))
-                   ;; For cljs, don't use cider-sync-tooling-eval, because Piggieback will later change the ns (issue #3503):
+                   ;; For cljs, don't use cider-tooling-eval, because Piggieback will later change the ns (issue #3503):
                    #'cider-nrepl-request:eval
-                 ;; When possible, favor cider-sync-tooling-eval because it preserves *1, etc (commit 5f705b):
-                 #'cider-sync-tooling-eval)))
+                 ;; When possible, favor cider-tooling-eval because it preserves *1, etc (commit 5f705b):
+                 #'cider-tooling-eval)))
         (funcall f (format "(in-ns '%s)" ns)
                  (cider-repl-switch-ns-handler connection))))))
 