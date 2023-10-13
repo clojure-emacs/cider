e9936f52432d25fdb2477dc4ec8fe95a7806e784
vemv <vemv@users.noreply.github.com>
2023-10-13 16:25:41 +0200

    Release CIDER 1.8.0 ("Geneva") (#3518)

--- cider.el
+++ cider.el
@@ -11,7 +11,7 @@
 ;;         Steve Purcell <steve@sanityinc.com>
 ;; Maintainer: Bozhidar Batsov <bozhidar@batsov.dev>
 ;; URL: http://www.github.com/clojure-emacs/cider
-;; Version: 1.8.0-snapshot
+;; Version: 1.8.0
 ;; Package-Requires: ((emacs "26") (clojure-mode "5.17.1") (parseedn "1.2.0") (queue "0.2") (spinner "1.7") (seq "2.22") (sesman "0.3.2") (transient "0.4.1"))
 ;; Keywords: languages, clojure, cider
 
@@ -93,7 +93,7 @@
 (require 'sesman)
 (require 'package)
 
-(defconst cider-version "1.8.0-snapshot"
+(defconst cider-version "1.8.0"
   "The current version of CIDER.")
 
 (defconst cider-codename "Geneva"
