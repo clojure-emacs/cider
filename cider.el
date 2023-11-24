4856be1af880bbc3d5ed3122640e601b1499587b
vemv <vemv@users.noreply.github.com>
2023-11-24 10:20:09 +0100

    Use enrich-classpath 1.18.6

--- cider.el
+++ cider.el
@@ -598,7 +598,7 @@ returned by this function does not include keyword arguments."
   (let ((plugins (if cider-enrich-classpath
                      (append cider-jack-in-lein-plugins
                              `(("cider/cider-nrepl" ,cider-injected-middleware-version)
-                               ("mx.cider/lein-enrich-classpath" "1.18.5")))
+                               ("mx.cider/lein-enrich-classpath" "1.18.6")))
                    (append cider-jack-in-lein-plugins
                            `(("cider/cider-nrepl" ,cider-injected-middleware-version))))))
     (thread-last
