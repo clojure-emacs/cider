6403a8d8b59f1e9f2e02cec976e577dd098f44c1
vemv <vemv@users.noreply.github.com>
2024-03-10 20:15:19 +0100

    Use cider-nrepl 0.47.0 (#3631)

--- dev/tramp-sample-project/project.clj
+++ dev/tramp-sample-project/project.clj
@@ -2,5 +2,5 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http "3.12.3"]]
   :source-paths ["src"]
-  :plugins [[cider/cider-nrepl "0.46.0"]
+  :plugins [[cider/cider-nrepl "0.47.0"]
             [refactor-nrepl "3.9.0"]])
