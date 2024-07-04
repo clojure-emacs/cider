1f8a041e25c280eeac37d516bc77eb3b8f0d40f9
Naoya Yamashita <conao3@gmail.com>
2024-07-05 06:55:51 +0900

    call cider-docstring--format after checking argument

--- cider-eldoc.el
+++ cider-eldoc.el
@@ -218,9 +218,10 @@ information."
          (symbol (lax-plist-get eldoc-info "symbol"))
          (docstring (or (cider--render-docstring-first-sentence eldoc-info)
                         (cider--render-docstring eldoc-info)
-                        (cider-docstring--trim
-                         (cider-docstring--format
-                          (lax-plist-get eldoc-info "docstring")))))
+                        (when (lax-plist-get eldoc-info "docstring")
+                          (cider-docstring--trim
+                           (cider-docstring--format
+                            (lax-plist-get eldoc-info "docstring"))))))
          ;; if it's a single class (and not multiple class candidates), that's it
          (maybe-class (car (lax-plist-get eldoc-info "class")))
          (formatted-var (or (when maybe-class
