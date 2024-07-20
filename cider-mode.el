6bd9ecf55a3b05044d5437eda917a3ddd5b28f8c
larstvei <larstvei@ifi.uio.no>
2024-07-20 15:17:47 +0200

    [Fix #3725] Use defvaralias for x-gtk-use-system-tooltips on emacs29

--- cider-mode.el
+++ cider-mode.el
@@ -1062,7 +1062,12 @@ property."
 
 
 ;;; Minor-mode definition
-(defvar x-gtk-use-system-tooltips)
+
+(if (and (> emacs-major-version 28)
+         (not (boundp 'x-gtk-use-system-tooltips)))
+    ;; The x-gtk prefix has been dropped in Emacs 29
+    (defvaralias 'x-gtk-use-system-tooltips 'use-system-tooltips)
+  (defvar x-gtk-use-system-tooltips))
 
 ;;;###autoload
 (define-minor-mode cider-mode
