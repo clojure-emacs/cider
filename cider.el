c5084901af4b9d5e6876d656d7229c318709ec99
Oleksandr Yakushev <alex@bytopia.org>
2025-04-14 18:44:04 +0300

    Bump cider-nrepl to 0.55.1

--- cider.el
+++ cider.el
@@ -573,7 +573,7 @@ the artifact.")
 
 Used when `cider-jack-in-auto-inject-clojure' is set to `latest'.")
 
-(defconst cider-required-middleware-version "0.55.0"
+(defconst cider-required-middleware-version "0.55.1"
   "The CIDER nREPL version that's known to work properly with CIDER.")
 
 (defcustom cider-injected-middleware-version cider-required-middleware-version
