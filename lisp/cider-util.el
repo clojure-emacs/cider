ccaa44be886636ab87952a222a16e39ee4954043
JenChieh <jcs090218@gmail.com>
2026-01-31 17:28:23 +0800

    fix: Try raw definition

--- lisp/cider-util.el
+++ lisp/cider-util.el
@@ -252,13 +252,13 @@ Can only error if SKIP is non-nil."
   "Extract PROP from PLIST using `equal'.
 
 An alternative `lax-plist-get' that got deprecated in Emacs 29."
-  (plist-get plist prop))
+  (plist-get plist prop #'equal))
 
 (defun cider-plist-put (plist prop val)
   "Change value in PLIST of PROP to VAL, comparing with `equal'.
 
 An alternative to `lax-plist-put' that got deprecated in Emacs 29."
-  (plist-put plist prop val))
+  (plist-put plist prop val #'equal))
 
 
 ;;; Text properties
