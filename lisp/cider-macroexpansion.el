82b5ebaeb75b6478ed7737fbff2ba0f593735d74
Bozhidar Batsov <bozhidar@batsov.dev>
2026-02-08 14:04:51 +0200

    Fix docstring quoting to use proper Emacs conventions
    Use backtick-apostrophe (\`symbol') for Lisp symbol references in
    docstrings instead of plain single quotes ('symbol), which triggers
    byte-compiler warnings.  For English phrases, either drop the quotes
    or use escaped quotes where literal code is shown.

--- lisp/cider-macroexpansion.el
+++ lisp/cider-macroexpansion.el
@@ -40,9 +40,9 @@
   "Determines if namespaces are displayed in the macroexpansion buffer.
 Possible values are:
 
-  'qualified ;=> Vars are fully-qualified in the expansion
-  'none      ;=> Vars are displayed without namespace qualification
-  'tidy      ;=> Vars that are :refer-ed or defined in the current namespace are
+  `qualified' ;=> Vars are fully-qualified in the expansion
+  `none'      ;=> Vars are displayed without namespace qualification
+  `tidy'      ;=> Vars that are :refer-ed or defined in the current namespace are
                  displayed with their simple name, non-referred vars from other
                  namespaces are referred using the alias for that namespace (if
                  defined), other vars are displayed fully qualified."
