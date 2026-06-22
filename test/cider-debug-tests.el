e89d466d8b354b27d38cbaac01b00f752cd65516
Bozhidar Batsov <bozhidar@batsov.dev>
2026-06-22 08:17:35 +0300

    Add tests for the enlighten overlay handler
    cider--handle-enlighten had no coverage. Exercise both render paths: a
    value overlay after a sexp and the local-face overlay on a bare symbol.

--- test/cider-debug-tests.el
+++ test/cider-debug-tests.el
@@ -178,3 +178,28 @@
       (save-excursion (insert "(defn a [] (let [x 1] #break ^{foo (foo x)} (inc x))"))
       (cider--debug-move-point '(3 2 1))
       (expect (thing-at-point 'symbol) :to-equal "x"))))
+
+(defun cider-debug-tests--enlighten-overlays ()
+  "Return the `enlighten' overlays in the current buffer."
+  (seq-filter (lambda (o) (eq (overlay-get o 'category) 'enlighten))
+              (overlays-in (point-min) (point-max))))
+
+(describe "cider--handle-enlighten"
+  (it "overlays the value after an enlightened sexp"
+    (with-temp-buffer
+      (clojure-mode)
+      (insert "(+ 1 2)")
+      ;; point (and the marker) sit right after the closing paren
+      (spy-on 'cider--debug-find-source-position :and-return-value (point-marker))
+      (cider--handle-enlighten (nrepl-dict "debug-value" "3"))
+      (expect (length (cider-debug-tests--enlighten-overlays)) :to-be-greater-than 0)))
+
+  (it "marks an enlightened local with the local face"
+    (with-temp-buffer
+      (clojure-mode)
+      (insert "x")
+      (spy-on 'cider--debug-find-source-position :and-return-value (point-marker))
+      (cider--handle-enlighten (nrepl-dict "debug-value" "42"))
+      (let ((ov (car (cider-debug-tests--enlighten-overlays))))
+        (expect ov :not :to-be nil)
+        (expect (overlay-get ov 'face) :to-equal 'cider-enlightened-local-face)))))
