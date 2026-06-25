0348939ebdfc3f2092f896c47ce3635aabbe3315
Bozhidar Batsov <bozhidar@batsov.dev>
2026-06-25 19:12:01 +0300

    Bump the injected cider-nrepl to 0.61.0-alpha1
    Makes today's ClojureScript wiring actually work out of the box: cljs
    test support, the `clojure-only' op signal, and the cljs macroexpansion
    fix all live in cider-nrepl 0.61.0-alpha1.

--- lisp/cider-jack-in.el
+++ lisp/cider-jack-in.el
@@ -322,7 +322,7 @@ the artifact.")
 
 Used when `cider-jack-in-auto-inject-clojure' is set to `latest'.")
 
-(defconst cider-required-middleware-version "0.60.0"
+(defconst cider-required-middleware-version "0.61.0-alpha1"
   "The CIDER nREPL version that's known to work properly with CIDER.")
 
 (defcustom cider-injected-middleware-version cider-required-middleware-version
