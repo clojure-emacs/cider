b110155d3f1888d73e7c84782d718a14c45d22eb
Bozhidar Batsov <bozhidar@batsov.dev>
2026-07-27 09:47:40 +0200

    Fold cider-error-parsing-tests.el into cider-compilation-tests.el
    All of its specs exercise cider-compilation.el symbols
    (cider-extract-error-info, the compilation regexes), which already have
    a home in cider-compilation-tests.el.

--- test/cider-compilation-tests.el
+++ test/cider-compilation-tests.el
@@ -100,6 +100,121 @@ when S doesn't match."
       (expect (cider-clojure-compilation-error-phases)
               :to-equal cider-clojure-compilation-error-phases-default-value))))
 
+;; Accessors for `cider-extract-error-info' results.
+(defun cider-error-test--file-name (info) (nth 0 info))
+(defun cider-error-test--line-num (info) (nth 1 info))
+(defun cider-error-test--col-num (info) (nth 2 info))
+(defun cider-error-test--face (info) (nth 3 info))
+
+(describe "cider-extract-error-info"
+  (it "extracts correct information from the error message"
+
+    ;; test-cider-extract-error-info-14
+    (let* ((message "Syntax error compiling at (/some/test/file/core.clj:31). Unable to resolve symbol: dummy in this context.")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-equal "/some/test/file/core.clj")
+      (expect (cider-error-test--line-num info) :to-equal 31)
+      (expect (cider-error-test--col-num info) :to-be nil)
+      (expect (cider-error-test--face info) :to-equal 'cider-error-highlight-face))
+
+    ;; test-cider-extract-error-info-14-windows
+    (let* ((message "Syntax error compiling at (c:\\some\\test\\file\\core.clj:31). Unable to resolve symbol: dummy in this context.")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-equal "c:\\some\\test\\file\\core.clj")
+      (expect (cider-error-test--line-num info) :to-equal 31)
+      (expect (cider-error-test--col-num info) :to-be nil)
+      (expect (cider-error-test--face info) :to-equal 'cider-error-highlight-face))
+
+    ;; test-cider-extract-error-info-14-no-file
+    (let* ((message "Syntax error compiling at (REPL:31). Unable to resolve symbol: dummy in this context.")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-be nil)
+      (expect (cider-error-test--line-num info) :to-equal 31)
+      (expect (cider-error-test--col-num info) :to-be nil)
+      (expect (cider-error-test--face info) :to-equal 'cider-error-highlight-face))
+
+
+    ;; test-cider-extract-warning-info-14
+    (let* ((message "Reflection warning, /some/othertest/file/core.clj:24 - reference to field getCanonicalPath can't be resolved.")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-equal "/some/othertest/file/core.clj")
+      (expect (cider-error-test--line-num info) :to-equal 24)
+      (expect (cider-error-test--col-num info) :to-be nil)
+      (expect (cider-error-test--face info) :to-equal 'cider-warning-highlight-face))
+
+    ;; test-cider-extract-warning-info-14-no-file
+    (let* ((message "Reflection warning, NO_SOURCE_PATH:24 - reference to field getCanonicalPath can't be resolved.")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-be nil)
+      (expect (cider-error-test--line-num info) :to-equal 24)
+      (expect (cider-error-test--col-num info) :to-be nil)
+      (expect (cider-error-test--face info) :to-equal 'cider-warning-highlight-face))
+
+    ;; test-cider-extract-error-info-15
+    (let* ((message "Syntax error compiling at (/some/test/file/core.clj:31:3). Unable to resolve symbol: dummy in this context.")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-equal "/some/test/file/core.clj")
+      (expect (cider-error-test--line-num info) :to-equal 31)
+      (expect (cider-error-test--col-num info) :to-equal 3)
+      (expect (cider-error-test--face info) :to-equal 'cider-error-highlight-face))
+
+    ;; test-cider-extract-error-info-15-no-file
+    (let* ((message "Syntax error compiling at (REPL:31:3). Unable to resolve symbol: dummy in this context")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-be nil)
+      (expect (cider-error-test--line-num info) :to-equal 31)
+      (expect (cider-error-test--col-num info) :to-equal 3)
+      (expect (cider-error-test--face info) :to-equal 'cider-error-highlight-face))
+
+    ;; test-cider-extract-warning-info-15
+    (let* ((message "Reflection warning, /some/othertest/file/core.clj:24:43 - reference to field getCanonicalPath can't be resolved.")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-equal "/some/othertest/file/core.clj")
+      (expect (cider-error-test--line-num info) :to-equal 24)
+      (expect (cider-error-test--col-num info) :to-equal 43)
+      (expect (cider-error-test--face info) :to-equal 'cider-warning-highlight-face))
+
+    ;; test-cider-extract-warning-info-15-no-file
+    (let* ((message "Reflection warning, NO_SOURCE_PATH:24:43 - reference to field getCanonicalPath can't be resolved.")
+           (info (cider-extract-error-info cider-compilation-regexp message)))
+      (expect (cider-error-test--file-name info) :to-be nil)
+      (expect (cider-error-test--line-num info) :to-equal 24)
+      (expect (cider-error-test--col-num info) :to-equal 43)
+      (expect (cider-error-test--face info) :to-equal 'cider-warning-highlight-face))))
+
+(describe "The cider compilation regexes"
+  (it "Recognizes a clojure warning message"
+    (let ((clojure-compiler-warning "Reflection warning, /tmp/foo/src/foo/core.clj:14:1 - call to java.lang.Integer ctor can't be resolved."))
+      (expect clojure-compiler-warning :to-match cider-clojure-compilation-regexp)
+      (expect (progn (string-match cider-clojure-compilation-regexp clojure-compiler-warning)
+                     (match-string 1 clojure-compiler-warning))
+              :to-equal "warning")))
+  ;; FIXME: duplicate spec names
+  (let ((regexp cider-clojure-compilation-regexp))
+    (it "Recognizes a clojure-1.10 error message"
+      (let ((clojure-1.10-compiler-error "Syntax error compiling at (src/ardoq/service/workspace_service.clj:227:3)."))
+        (expect clojure-1.10-compiler-error :to-match regexp)
+        (expect (progn (string-match regexp clojure-1.10-compiler-error)
+                       (match-string 2 clojure-1.10-compiler-error))
+                :to-equal "src/ardoq/service/workspace_service.clj")))
+    (it "Recognizes a clojure 'Unexpected error' message"
+      (let ((clojure-1.10-compiler-error "Unexpected error (ClassCastException) macroexpanding defmulti at (src/haystack/parser.cljc:21:1)."))
+        (expect clojure-1.10-compiler-error :to-match regexp)
+        (expect (progn (string-match regexp clojure-1.10-compiler-error)
+                       (match-string 2 clojure-1.10-compiler-error))
+                :to-equal "src/haystack/parser.cljc")))))
+
+(describe "cider-module-info-regexp"
+  (it "Matches module info provided by Java"
+    (expect " (java.lang.Long is in module java.base of loader 'bootstrap'; clojure.lang.IObj is in unnamed module of loader 'app')"
+            :to-match cider-module-info-regexp)
+    (expect " (java.lang.Long is in module java.base of loader 'bootstrap'; clojure.lang.IObj is in module java.base of loader 'bootstrap')"
+            :to-match cider-module-info-regexp)
+    (expect " (java.lang.Long is in unnamed module of loader 'app'; clojure.lang.IObj is in module java.base of loader 'bootstrap')"
+            :to-match cider-module-info-regexp)
+    (expect " (java.lang.Long is in unnamed module of loader 'app'; clojure.lang.IObj is in unnamed module of loader 'app')"
+            :to-match cider-module-info-regexp)))
+
 (provide 'cider-compilation-tests)
 
 ;;; cider-compilation-tests.el ends here
